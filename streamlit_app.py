b1036abb7c668aed722908f340c85c18ff21dec4
Shaurya <135049227+ShauryaS31@users.noreply.github.com>
2024-08-09 12:53:55 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,8 @@ clf.fit(X ,y)
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 df_prediction_proba = pd.DataFrame(prediction_proba)
-
+df_prediction_proba.column = ["Adelie", "Chinstrap", "Gentoo"]
+df_prediction_proba
 
 
 
